3cd8f31754e2f7ba1b53efa3c1cd32a4ccb7ae68
byrongibby <byron@byrongibby.online>
2022-03-07 10:25:07 +0200

    Added code for pulling data in for chart

---
+++ post 2/R/chart.R
@@ -0,0 +1,31 @@
+library(econdatar)
+library(dplyr)
+library(ggplot2)
+library(zoo)
+
+ba100 <- read_econdata(agencyid = "ECONDATA",
+                                    id = "BA100",
+                                    key = "TOT..L024",
+                                    releasedescription = "Mar 2022")
+
+# Series returned from EconData
+names(ba100)
+
+# Data set and time series metadata
+attributes(ba100)$metadata
+attributes(ba100$TOT.A3.L024)$metadata
+
+# Time series data
+head(ba100$TOT.A3.L024)
+
+# Convert time series to tibble and calculate YoY growth rate
+(loans_and_advances <- tibble(Period = rownames(ba100$TOT.A3.L024) %>%
+                                         as.Date() %>%
+                                         as.yearmon(),
+                              Value = ba100$TOT.A3.L024$OBS_VALUE) %>%
+   mutate(Value = (Value / dplyr::lag(Value, n = 12) - 1) * 100) %>%
+   filter(!is.na(Value)))
+
+# Plot YoY loans and advances
+ggplot(data = loans_and_advances, aes(x = Period, y = Value)) +
+  geom_line()
