fa9a81bfc436887ffbf57233c4e657221f6ba699
byrongibby <byron@byrongibby.online>
2022-03-07 10:27:05 +0200

    Removed zoo dependency

--- post 2/R/chart.R
+++ post 2/R/chart.R
@@ -1,7 +1,6 @@
 library(econdatar)
 library(dplyr)
 library(ggplot2)
-library(zoo)
 
 ba100 <- read_econdata(agencyid = "ECONDATA",
                                     id = "BA100",
@@ -19,9 +18,7 @@ attributes(ba100$TOT.A3.L024)$metadata
 head(ba100$TOT.A3.L024)
 
 # Convert time series to tibble and calculate YoY growth rate
-(loans_and_advances <- tibble(Period = rownames(ba100$TOT.A3.L024) %>%
-                                         as.Date() %>%
-                                         as.yearmon(),
+(loans_and_advances <- tibble(Period = as.Date(rownames(ba100$TOT.A3.L024)),
                               Value = ba100$TOT.A3.L024$OBS_VALUE) %>%
    mutate(Value = (Value / dplyr::lag(Value, n = 12) - 1) * 100) %>%
    filter(!is.na(Value)))
