6e3f02b5f856d59d681904248961598c5508e35a
aidanhorn <aidanhorn@gmail.com>
2025-03-25 11:06:17 +0200

    GDP vintages blog post update

--- statssa/gdp/vintages/R/data.R
+++ statssa/gdp/vintages/R/data.R
@@ -1,52 +1,47 @@
+# GDP vintages data ---
+# https://econdata.co.za/making-use-of-vintage-data/
+
+if (TRUE) {
 library(econdatar)
 library(dplyr)
 library(ggplot2)
 library(readr)
 library(tidyr)
-
-
-
-# GDP vintages data ---
-
-
-format_gdp <- function(x) {
-  as.yearqtr <- function(y)
-    paste0(format(y, format = "%Y"), quarters(y))
-  tibble(Value = x$KBP6006.R.S$OBS_VALUE) %>%
-    mutate(Date = x$KBP6006.R.S %>%
-           rownames() %>%
-           as.Date() %>%
-           as.yearqtr()) %>%
-  mutate(Vintage = release$Description)
+library(stringr)
 }
 
 releases <- read_release(id = "NATL_ACC",
-                         agencyid = "ECONDATA",
-                         version = "all",
-                         provideragencyid = "ECONDATA",
-                         providerid = "STATSSA")
+                         version = "all")
 
-gdp_vintages <- NULL
-
-for (dataset in releases) {
-  for(release in dataset$Releases) {
-    print(release$Description)
-
-    natl_acc <- read_econdata(agencyid = "ECONDATA",
-                              id = "NATL_ACC",
-                              version = dataset$DataSet$Flowref[[3]],
-                              key = "KBP6006.R.S",
-                              provideragencyid = "ECONDATA",
-                              providerid = "STATSSA",
-                              releasedescription = release$Description)
-
-    gdp_vintages <- rbind(gdp_vintages, format_gdp(natl_acc))
+gdp_vintages <- tibble()
+for (data_i in seq_len(length(releases))) {
+  data_version <- names(releases)[[data_i]]
+  cat(paste("\n", data_version, "\n"))
+  dataset <- releases[[data_i]]
+  for(release_i in seq_len(nrow(dataset))) {
+    i <- slice(dataset, release_i)
+    cat(paste(i$description, "\n"))
+    natl_acc <- read_dataset(id = "NATL_ACC",
+                            version = str_sub(data_version, 2, -1),
+                            series_key = ifelse(as.numeric(str_sub(data_version, 2, 2)) >= 2,
+                                        "PROD.GDP.Q.R.S",
+                                        "KBP6006.R.S"),
+                            release = i$description,
+                            tidy = TRUE,
+                            wide = FALSE,
+                            combine = TRUE) %>%
+        as_tibble() %>%
+        mutate(description = i$description,
+              release = i$release) %>%
+        relocate(release, description, time_period, obs_value, series_name, series_key)
+    gdp_vintages <- bind_rows(gdp_vintages, natl_acc)
   }
 }
+gdp_vintages <- gdp_vintages %>%
+  arrange(release, time_period, series_key)
 
 write_csv(gdp_vintages %>%
-          arrange(Vintage, Date) %>%
           pivot_wider(id_cols = everything(),
-                      names_from = Vintage,
-                      values_from = Value),
-          "data/Realtime GDP.csv")
+                      names_from = description,
+                      values_from = obs_value),
+          "realtimeGDP.csv")
