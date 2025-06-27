9d1dfd1bb39ae1bc7b9bd645f646c0ed17cddc0d
Aidan Horn <hello@aidanhorn.co.za>
2025-06-27 14:41:52 +0200

    Example extraction: update parameter name

--- docs/concept-checker.R
+++ docs/concept-checker.R
@@ -1,15 +1,15 @@
 library(econdatar)
 library(tidyverse)
 
-data_raw <- read_dataset(id = "PPI",
-                         releasedescription = "latest",
+data_raw <- read_dataset(id = "BUSINESS_CYCLES",
                          tidy = TRUE,
                          wide = FALSE,
-                         compact = FALSE,
-                         version = "latest")
+                         compact = FALSE)
 str(data_raw)
 print(names(data_raw$data))
 print(names(data_raw$metadata))
+
+as_tibble(data_raw$metadata) %>% print(width = 700L)
 
 neat <- as_tibble(data_raw$metadata) %>%
     select(label, series_key, base_period, unit_of_measure, seasonal_adjustment, frequency) %>%
