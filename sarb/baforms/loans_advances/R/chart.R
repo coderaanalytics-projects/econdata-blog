928b6e3f629ff3e53dfe7326928d14b8d2cf14ab
Aidan Horn <hello@aidanhorn.co.za>
2025-07-10 10:13:44 +0200

    Introduction blog post update

--- sarb/baforms/loans_advances/R/chart.R
+++ sarb/baforms/loans_advances/R/chart.R
@@ -1,43 +1,40 @@
+# https://econdata.co.za/charting-and-automation/
+
 library(econdatar)
 library(dplyr)
 library(ggplot2)
 library(gridExtra)
 
-ba100 <- read_econdata(id = "BA100",
-                       key = "TOT..L024",
-                       releasedescription = "Mar 2022")
+ba100 <- read_dataset(id = "BA100",
+                      series_key = "TOT..L024")
 
 # Series returned from EconData
 names(ba100)
 
 # Data set and time series metadata
 attributes(ba100)$metadata
-attributes(ba100$TOT.A3.L024)$metadata
+attributes(ba100$TOT.A3.L024)
 
 # Time series data
 head(ba100$TOT.A3.L024)
 
 # Convert time series to tibble and calculate YoY growth rate
-(loans_and_advances <- tibble(Period = as.Date(rownames(ba100$TOT.A3.L024)),
-                              Value = ba100$TOT.A3.L024$OBS_VALUE,
-                              Label = "loans") %>%
-   mutate(Value = (Value / dplyr::lag(Value, n = 12) - 1) * 100) %>%
-   filter(!is.na(Value)))
+loans_and_advances <- as_tibble(ba100) %>%
+    arrange(time_period) %>%
+    mutate(growth = (TOT.A3.L024 / dplyr::lag(TOT.A3.L024, n = 12) - 1) * 100) %>%
+    filter(!is.na(growth))
 
 # Plot YoY loans and advances
-(p <- ggplot(data = loans_and_advances) +
-  geom_line(aes(x = Period, y = Value), colour = "#273b8d", size = 2) +
-  labs(caption = "Source: EconData") +
+p <- ggplot(data = loans_and_advances) +
+  geom_line(aes(x = time_period, y = growth), colour = "#273b8d", linewidth = 2) +
+  labs(caption = "Source: www.econdata.co.za") +
   xlab("") + ylab("%") +
   theme_classic(base_size = 14) +
-  theme(panel.grid.major.x = element_blank(),
-        panel.grid.minor.x = element_blank(),
-        panel.grid.major.y = element_blank(),
-        panel.grid.minor.y = element_blank(),
-        axis.text = element_text(face = "bold")))
+  theme(panel.grid = element_blank(),
+        axis.text = element_text(face = "bold"))
 
 # Save plot as PNG
-ggsave("loans and advances.png",
+ggsave("sarb/baforms/loans_advances/inst/loans and advances.png",
        height = 4.5,
        width = 6,
        plot = p,
