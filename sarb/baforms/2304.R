fc9cee9b1a7ff1d0f70a0f15393377c4a60a5bd0
Aidan Horn <hello@aidanhorn.co.za>
2026-07-02 14:43:42 +0200

    BA700 extraction

--- sarb/baforms/2304.R
+++ sarb/baforms/2304.R
@@ -1,33 +1,41 @@
-# BA900 extraction: Redmine Issue 2304
+# BA form extractions: Redmine Issue 2304
 
 library(econdatar)
 library(tidyverse)
 library(openxlsx)
 
-ba900 <- read_dataset(id = "BA900",
-                      series_key = "TOT+NED+ABS+FRB+STB..",
-                      tidy = TRUE,
-                      wide = TRUE) %>%
-    as_tibble()
+export_baform <- function(id, v = "latest", outfile) {
+    data <- read_dataset(id = id,
+                         version = v,
+                         series_key = "TOT+NED+ABS+FRB+STB..",
+                         tidy = TRUE,
+                         wide = TRUE) %>%
+        as_tibble()
 
-banks <- names(ba900)[-1] %>%
-    sub("^([^.]+)\\..*", "\\1", .) %>%
-    unique()
+    banks <- names(data)[-1] %>%
+        sub("^([^.]+)\\..*", "\\1", .) %>%
+        unique()
 
-column_labels <- function(cols) {
-    map_chr(cols, \(col) attr(ba900[[col]], "label") %||% "")
+    column_labels <- function(cols) {
+        map_chr(cols, \(col) attr(data[[col]], "label") %||% "")
+    }
+
+    wb <- createWorkbook()
+
+    walk(banks, \(bank) {
+        bank_cols <- grep(paste0("^", bank, "\\."), names(data), value = TRUE)
+        sheet_data <- data %>% select(time_period, all_of(bank_cols))
+
+        addWorksheet(wb, bank)
+        writeData(wb, bank, t(names(sheet_data)), startRow = 1, colNames = FALSE)
+        writeData(wb, bank, t(column_labels(names(sheet_data))), startRow = 2, colNames = FALSE)
+        writeData(wb, bank, sheet_data, startRow = 3, colNames = FALSE)
+    })
+
+    saveWorkbook(wb, outfile, overwrite = TRUE)
 }
 
-wb <- createWorkbook()
+export_baform("BA900", "../ba900.xlsx")
+export_baform("BA700", "../ba700.xlsx")
 
-walk(banks, \(bank) {
-    bank_cols <- grep(paste0("^", bank, "\\."), names(ba900), value = TRUE)
-    sheet_data <- ba900 %>% select(time_period, all_of(bank_cols))
-
-    addWorksheet(wb, bank)
-    writeData(wb, bank, t(names(sheet_data)), startRow = 1, colNames = FALSE)
-    writeData(wb, bank, t(column_labels(names(sheet_data))), startRow = 2, colNames = FALSE)
-    writeData(wb, bank, sheet_data, startRow = 3, colNames = FALSE)
-})
-
-saveWorkbook(wb, "../ba900.xlsx", overwrite = TRUE)
+export_baform("BA700", v = "1.0.0", "../ba700_1.0.0.xlsx")
