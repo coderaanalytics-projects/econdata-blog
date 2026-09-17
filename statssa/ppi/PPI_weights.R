27ca7506f12c03dfa52724a07aa367db6cfd4d70
Aidan Horn <hello@aidanhorn.co.za>
2026-09-17 15:26:30 +0200

    Update PPI weights baskets

--- statssa/ppi/PPI_weights.R
+++ statssa/ppi/PPI_weights.R
@@ -1,5 +1,4 @@
 # Live PPI weights from EconData
-# Output: statssa/ppi/ppi_weights.csv
 
 if (TRUE) {
   library(econdatar)
@@ -15,6 +14,7 @@ ppi <- read_dataset("PPI",
 weights <- as_tibble(ppi$metadata) %>%
   transmute(series_key,
             mnemonic,
+            basket,
             activity,
             source_identifier,
             label,
@@ -22,11 +22,15 @@ weights <- as_tibble(ppi$metadata) %>%
             weight = as.numeric(weight)) %>%
   filter(!is.na(weight))
 
-weight_sum <- sum(weights$weight)
-message("Weight sum: ", weight_sum)
-stopifnot(abs(weight_sum - 500) < 0.01)
+agriculture <- filter(weights, basket == "AGRICULTURE")
+mining <- filter(weights, basket == "MINING")
+final_manufactured <- filter(weights, basket == "FINAL")
+intermediate <- filter(weights, basket == "INTERMEDIATE")
+electricity_and_water <- filter(weights, basket == "ELECTRICITY")
 
-write.csv(weights,
-          file = "statssa/ppi/ppi_weights.csv",
-          na = "",
-          row.names = FALSE)
+write.csv(weights, "statssa/ppi/ppi_weights.csv", na = "", row.names = FALSE)
+write.csv(agriculture, "statssa/ppi/ppi_agriculture.csv", na = "", row.names = FALSE)
+write.csv(mining, "statssa/ppi/ppi_mining.csv", na = "", row.names = FALSE)
+write.csv(final_manufactured, "statssa/ppi/ppi_final_manufactured.csv", na = "", row.names = FALSE)
+write.csv(intermediate, "statssa/ppi/ppi_intermediate_manufactured.csv", na = "", row.names = FALSE)
+write.csv(electricity_and_water, "statssa/ppi/ppi_electricity_and_water.csv", na = "", row.names = FALSE)
